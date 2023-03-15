570178746472ccece724b35d3acdf0dcf46f2a9e
CCherry07 <2405693142@qq.com>
2023-03-15 13:24:48 +0800

    feat: projects

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -27,8 +27,17 @@ i18n
           "contactMeDescription3": "Thank you for your support.",
           "OVERVIEW": "OVERVIEW",
           "INTRODUCTION": "INTRODUCTION",
+
+          "Web Developer": "Web Developer",
+          "React Developer": "React Developer",
+          "Vue Developer": "Vue Developer",
+          "Content Creator": "Content Creator",
+
           "What i have done so far": "What i have done so far",
           "Work Experience": "Work Experience",
+          "My Work": "My Work",
+          "Projects": "Projects",
+          "Projects Dec": "Following projects showcases my skills and experience through real-world examples of my work. Each project is briefly described with links to code repositories and live demos in it. It reflects my ability to solve complex problems, work with different technologies, and manage projects effectively."
         }
       },
       zh: {
@@ -53,8 +62,17 @@ i18n
           "contactMeDescription3": "感谢您的支持。",
           "OVERVIEW": "概述",
           "INTRODUCTION": "简介",
+
+          "Web Developer": "网页开发者",
+          "React Developer": "React 开发者",
+          "Vue Developer": "Vue 开发者",
+          "Content Creator": "内容创作者",
+
           "What i have done so far": "我做过的事情",
           "Work Experience": "工作经验",
+          "My Work": "我的工作",
+          "Projects": "项目集",
+          "Projects Dec": "以下项目通过我工作的真实示例展示了我的技能和经验。每个项目都通过指向代码存储库和其中的现场演示的链接进行了简要描述。它反映了我解决复杂问题、使用不同技术以及有效管理项目的能力"
         }
       }
     },
